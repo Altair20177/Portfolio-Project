8c90aedabe5f6abe78921ce4d5d49af788d210d2
Maksim <78382480+Altair20177@users.noreply.github.com>
2022-01-14 22:21:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const seasonsBtns = document.querySelectorAll('.season__btn');
 const languages = document.querySelectorAll('.header__navbar-language span');
 
+const burger = document.querySelector('.burger');
+const container = document.querySelector('.container');
+const cross = document.querySelector('.cross');
+const sideMenu = document.querySelector('aside');
+
 seasonsBtns.forEach(seasonBtn => {
     seasonBtn.addEventListener('click', () => {
         seasonsBtns.forEach( btn => {btn.classList.remove("active__season")});
@@ -14,3 +19,15 @@ languages.forEach(activeLang => {
         if(!activeLang.classList.contains("active__language")) activeLang.classList.add("active__language");
     })
 });
+
+
+
+burger.onclick = () => {
+    sideMenu.classList.add('active__side-menu');
+    container.classList.add('active__side-menu');
+}
+
+cross.onclick = () => {
+    sideMenu.classList.remove('active__side-menu');
+    container.classList.remove('active__side-menu');
+}
